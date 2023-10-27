9644ee71c10012a0516f7322e3bf1b5327ac3f1f
jeffubayi <jeffubayi@gmail.com>
2023-10-27 16:55:26 +0300

    add theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import createEmotionCache from '../utility/createEmotionCache';
+import ThemeOptions from '../styles/theme';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+type AppPropsWithLayout = AppProps & {
+  emotionCache: EmotionCache
+}
+
+export default function App({ Component, emotionCache = clientSideEmotionCache, pageProps }: AppPropsWithLayout) {
+  return (
+    // <CacheProvider value={emotionCache}>
+    //   <ThemeProvider theme={ThemeOptions}>
+    //     <CssBaseline />
+        <Component {...pageProps} />
+    //   </ThemeProvider>
+    // </CacheProvider>
+  )
 }
