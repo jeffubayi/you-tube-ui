b2b8f4b4fa96668f3aeab26e782c7de1e79b5ed0
jeffubayi <jeffubayi@gmail.com>
2023-11-07 03:53:28 +0300

    add enums

---
+++ utility/data.tsx
@@ -0,0 +1,128 @@
+import Settings from '@mui/icons-material/SettingsOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import FeedbackOutlinedIcon from '@mui/icons-material/FeedbackOutlined';
+import WhatshotIcon from '@mui/icons-material/WhatshotOutlined';
+import MusicNoteIcon from '@mui/icons-material/MusicNoteOutlined';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsportsOutlined';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEventsOutlined';
+
+
+export const data = [
+    {
+        src: 'https://i.ytimg.com/vi/sBws8MSXN7A/maxresdefault.jpg',
+        title: 'React JS Crash Course',
+        channel: 'Traversy Media',
+        views: '3.2M views',
+        createdAt: '2 years ago',
+        subs: '2M',
+        id: "w7ejDZ8SWv8&t=5111s",
+        avatar: 'https://yt3.ggpht.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s88-c-k-c0x00ffffff-no-rj',
+        desc: "Get started with React in this crash course. We will be building a task tracker app and look at components, props, state, hooks, working with an API and more. Code:https://github.com/bradtraversy/react..."
+    },
+    {
+        src: 'https://i.ytimg.com/vi/tvTRZJ-4EyI/maxresdefault.jpg',
+        title: 'Kendrick Lamar - HUMBLE',
+        channel: 'Kendrick Lamar',
+        views: '935M views',
+        subs: '12.3M',
+        createdAt: '6 years ago',
+        id: "tvTRZJ-4EyI",
+        avatar: "https://yt3.googleusercontent.com/V4FqOieQ9y9dnErXPUZNWl1hyLafxIK7F55n5M8LVhPBmEou8kAbNuMlUZx23DoJHvH1sWG56No=s176-c-k-c0x00ffffff-no-rj-mo",
+        desc: "Kendrick Lamar DAMN. Available now http://smarturl.it/DAMN Prod: Anthony Top Dawg Tiffith, Dave Free Nathan K. Scherrer, Jason Baum, Jamie Rabineau"
+    },
+    {
+        src: 'https://pbs.twimg.com/media/Fx64bHkaUAAEcQ_.jpg:large',
+        title: "Apple Vision Pro Impressions! ",
+        channel: 'Marques Brownlee',
+        views: '17M views',
+        createdAt: '10 hours ago',
+         subs: '17.3M',
+        id: "OFvXuyITwBI&t=405s",
+        avatar: "https://yt3.googleusercontent.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s176-c-k-c0x00ffffff-no-rj",
+        desc: "I tried Apple's first ever spatial computing device, a $3500 VR headset. These are my honest thoughts.. "
+    },
+
+    {
+        src: 'https://i.ytimg.com/vi/5MuIMqhT8DM/maxresdefault.jpg',
+        title: 'Sleep is your superpower',
+        channel: 'TED',
+        views: '11M views',
+        createdAt: '10 months ago',
+        subs: '23.5M',
+        id: "5MuIMqhT8DM",
+        avatar: "https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg",
+        desc: "Sleep is your life-support system and Mother Nature's best effort yet at immortality, says sleep scientist Matt Walker. In this deep dive into the science of slumber, Walker shares the wonderfully good things that happen when you get sleep -- and the alarmingly bad things that happen when you don't, for both your brain and body. Learn more about sleep's impact on your learning, memory. "
+    },
+    {
+        src: 'https://i.ytimg.com/vi/mwKJfNYwvm8/maxresdefault.jpg',
+        title: 'I Built 100 Wells In Africa',
+        channel: 'Mr Beast',
+        views: '60M views',
+        createdAt: '10 days ago',
+        id: "mwKJfNYwvm8",
+        subs:"207M",
+        avatar: 'https://yt3.ggpht.com/ytc/APkrFKbAfC_5NBQ3CM6Lyd2zXUFbC4mym1GCRhmpIwcMXg=s88-c-k-c0x00ffffff-no-rj',
+        desc: "Click the link below to donate  https://www.beastphilanthropy.org/cam... "
+      },
+      {
+        src: 'https://i.ytimg.com/vi/myjEoDypUD8/maxresdefault.jpg',
+        title: 'Will Smith smacks Chris Rock on stage at the Oscars',
+        channel: 'Guardian News',
+        views: '115M views',
+        createdAt: '1 year ago',
+        id: "myjEoDypUD8",
+        subs:"3.37",
+        avatar: 'https://yt3.ggpht.com/ytc/APkrFKZJp29eCTZGHL0n-btY1Ng5Jjfi2prBbcAoPuZo5Q=s88-c-k-c0x00ffffff-no-rj',
+        desc: "Best actor nominee Will Smith appeared to slap presenter Chris Rock in the face with an open hand and shouted a vulgarity at the comedian for making a joke about his wife's appearance at the Oscars ceremony. "
+      },
+];
+
+export const explore = [
+    {
+      icon: <WhatshotIcon sx={{ stroke: "#ffffff", strokeWidth: 1, fontSize: 25, fill: "#000" }} />,
+      title: 'Trending',
+  
+    },
+    {
+      icon: <MusicNoteIcon sx={{ stroke: "#ffffff", strokeWidth: 1, fontSize: 25, fill: "#000" }} />,
+      title: 'Music',
+  
+    },
+    {
+      icon: <SportsEsportsIcon sx={{ stroke: "#ffffff", strokeWidth: 1, fontSize: 25, fill: "#000" }} />,
+      title: 'Games',
+  
+    },
+    {
+      icon: <EmojiEventsIcon sx={{ stroke: "#ffffff", strokeWidth: 1, fontSize: 25, fill: "#000" }} />,
+      title: 'Sports',
+  
+    },
+  
+  
+  ];
+  
+  export const sets = [
+    {
+      icon: <Settings sx={{ stroke: "#ffffff", strokeWidth: 1, fontSize: 25, fill: "#000" }} />,
+      title: 'Settings',
+  
+    },
+    {
+      icon: <MusicNoteIcon sx={{ stroke: "#ffffff", strokeWidth: 1, fontSize: 25, fill: "#000" }} />,
+      title: 'Report history',
+  
+    },
+    {
+      icon: <HelpOutlineOutlinedIcon sx={{ stroke: "#ffffff", strokeWidth: 1, fontSize: 25, fill: "#000" }} />,
+      title: 'Help',
+  
+    },
+    {
+      icon: <FeedbackOutlinedIcon sx={{ stroke: "#ffffff", strokeWidth: 1, fontSize: 25, fill: "#000" }} />,
+      title: 'Send feedback',
+  
+    },
+  
+  
+  ];
