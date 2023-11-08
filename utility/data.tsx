95e5aa786ab5907f4aa7e31aaad0de97242aa1b6
jeffubayi <jeffubayi@gmail.com>
2023-11-08 14:34:58 +0300

    update db

--- utility/data.tsx
+++ utility/data.tsx
@@ -16,6 +16,7 @@ export const data = [
         createdAt: '2 years ago',
         subs: '2M',
         id: "w7ejDZ8SWv8&t=5111s",
+        list:0,
         avatar: 'https://yt3.ggpht.com/ytc/APkrFKYcYswt_UhD7D0j6ddiQz6Gb8Q_vSJOjhYI0CoXSw=s88-c-k-c0x00ffffff-no-rj',
         desc: "Get started with React in this crash course. We will be building a task tracker app and look at components, props, state, hooks, working with an API and more. Code:https://github.com/bradtraversy/react..."
     },
@@ -27,6 +28,7 @@ export const data = [
         subs: '12.3M',
         createdAt: '6 years ago',
         id: "tvTRZJ-4EyI",
+        list:1,
         avatar: "https://yt3.googleusercontent.com/V4FqOieQ9y9dnErXPUZNWl1hyLafxIK7F55n5M8LVhPBmEou8kAbNuMlUZx23DoJHvH1sWG56No=s176-c-k-c0x00ffffff-no-rj-mo",
         desc: "Kendrick Lamar DAMN. Available now http://smarturl.it/DAMN Prod: Anthony Top Dawg Tiffith, Dave Free Nathan K. Scherrer, Jason Baum, Jamie Rabineau"
     },
@@ -38,6 +40,7 @@ export const data = [
         createdAt: '10 hours ago',
          subs: '17.3M',
         id: "OFvXuyITwBI&t=405s",
+        list:2,
         avatar: "https://yt3.googleusercontent.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s176-c-k-c0x00ffffff-no-rj",
         desc: "I tried Apple's first ever spatial computing device, a $3500 VR headset. These are my honest thoughts.. "
     },
@@ -50,6 +53,7 @@ export const data = [
         createdAt: '10 months ago',
         subs: '23.5M',
         id: "5MuIMqhT8DM",
+        list:3,
         avatar: "https://pbs.twimg.com/profile_images/877631054525472768/Xp5FAPD5_reasonably_small.jpg",
         desc: "Sleep is your life-support system and Mother Nature's best effort yet at immortality, says sleep scientist Matt Walker. In this deep dive into the science of slumber, Walker shares the wonderfully good things that happen when you get sleep -- and the alarmingly bad things that happen when you don't, for both your brain and body. Learn more about sleep's impact on your learning, memory. "
     },
@@ -60,6 +64,7 @@ export const data = [
         views: '60M views',
         createdAt: '10 days ago',
         id: "mwKJfNYwvm8",
+        list:4,
         subs:"207M",
         avatar: 'https://yt3.ggpht.com/ytc/APkrFKbAfC_5NBQ3CM6Lyd2zXUFbC4mym1GCRhmpIwcMXg=s88-c-k-c0x00ffffff-no-rj',
         desc: "Click the link below to donate  https://www.beastphilanthropy.org/cam... "
@@ -71,6 +76,7 @@ export const data = [
         views: '115M views',
         createdAt: '1 year ago',
         id: "myjEoDypUD8",
+        list:5,
         subs:"3.37",
         avatar: 'https://yt3.ggpht.com/ytc/APkrFKZJp29eCTZGHL0n-btY1Ng5Jjfi2prBbcAoPuZo5Q=s88-c-k-c0x00ffffff-no-rj',
         desc: "Best actor nominee Will Smith appeared to slap presenter Chris Rock in the face with an open hand and shouted a vulgarity at the comedian for making a joke about his wife's appearance at the Oscars ceremony. "
