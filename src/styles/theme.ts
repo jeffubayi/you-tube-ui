959922d755618935d8a74048e623999b4c5ae2c4
jeffubayi <jeffubayi@gmail.com>
2023-11-08 14:37:35 +0300

    styles:update theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -13,7 +13,8 @@ const theme = createTheme({
   palette: {
     primary: { main: "#15192D" },
     secondary: { main: "#EBEBEB" },
-    warning: { main: "#c1004b" },
+    warning: { main: "#FE0100" },
+    info: { main:  "#095ED5" },
     text: {
       primary: "#000",
       secondary: "grey",
@@ -25,6 +26,43 @@ const theme = createTheme({
   },
   typography: {
     fontFamily: roboto.style.fontFamily,
+    body1: {
+      fontSize: "10pt",
+    },
+  },
+  components: {
+    MuiListItemButton: {
+      styleOverrides: {
+        root: {
+          paddingTop: "8.5px",
+          paddingBottom: "8.5px",
+          "& .MuiListItemIcon-root": {
+            fontSize: "medium",
+          },
+          "&.Mui-selected": {
+            fontWeight: "bold",
+            borderRadius: "0.5rem ",
+            color: "#0000",
+            padding: "8px",
+            "& .MuiListItemIcon-root": {
+              color: "#00acc1",
+              fontSize: "medium",
+            },
+            "& .MuiTypography-root": {
+              fontWeight: "bold",
+            },
+          },
+          "&:hover": {
+            borderRadius: "0.7rem ",
+            fontWeight: "bold",
+            color:  "black",
+            "& .MuiListItemIcon-root": {
+              color: "#0000",
+            },
+          },
+        },
+      },
+    },
   },
 });
 
